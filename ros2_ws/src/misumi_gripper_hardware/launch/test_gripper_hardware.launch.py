25ff55d421b22b231dad681e75dc0a3d4e37aca8
dikeke <dikeke@qiniu.com>
2025-08-04 11:03:13 +0800

    test

--- ros2_ws/src/misumi_gripper_hardware/launch/test_gripper_hardware.launch.py
+++ ros2_ws/src/misumi_gripper_hardware/launch/test_gripper_hardware.launch.py
@@ -85,7 +85,7 @@ def generate_launch_description():
         output='both',
        parameters=[
         {
-            'robot_description': robot_description_content,
+            'robot_description': ParameterValue(robot_description_content, value_type=str),
             # 添加下面这个参数！
             'frame_prefix': 'misumi/'
         }
