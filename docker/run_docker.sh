b33d05ff42a9b21d63551c555611e051370fa671
dikeke <dikeke@qiniu.com>
2025-12-19 15:53:57 +0800

    test

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #IMAGE_NAME="eu_motor_base:latest"
-IMAGE_NAME="eu_motor_base:latest"
+IMAGE_NAME="misumi_base:ros2"
 docker run -it \
     -v "$(pwd):/misumi_gripper" \
     $IMAGE_NAME \
